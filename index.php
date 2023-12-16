aa2d0d34ac8eecbe79ae146dea94f3f7e63e1936
Alkhatep <abdulla.sayed94@gmail.com>
2023-12-16 15:27:09 +0200

    environments and allowing for flecibility

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ require 'functions.php';
 require 'Database.php';
 // require 'router.php';
 
-$db = new Database();
+$config = require('config.php');
 
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts")->fetchAll();
 
 
 
