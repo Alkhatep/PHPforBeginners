eefa7cbfbf9b90ccb5639445e5f207284e44a9e4
Alkhatep <abdulla.sayed94@gmail.com>
2023-12-16 20:10:15 +0200

    Rendering all of a user notes

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,6 +10,7 @@
                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                   <a href="/" class="<?= urlIS('/') ? 'bg-gray-900 text-white' : 'text-gray-300'?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
                   <a href="/about" class="<?= urlIS('/about') ? 'bg-gray-900 text-white' : 'text-gray-300'?> r hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                  <a href="/notes" class="<?= urlIS('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300'?> r hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Notes</a>
                   <a href="/contact" class="<?= urlIS('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300'?> r hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                 </div>
               </div>
