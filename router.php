3c26d137ffcf444968f2e840e300934fb9dbf805
Alkhatep <abdulla.sayed94@gmail.com>
2023-12-19 03:59:17 +0200

    intro to forms

--- router.php
+++ router.php
@@ -1,16 +1,7 @@
 <?php
 
+$routes = require('routes.php');
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-	'/' => 'controllers/index.php',
-	'/about' => 'controllers/about.php',
-	'/notes' => 'controllers/notes.php',
-	'/note' => 'controllers/note.php',
-	'/contact' => 'controllers/contact.php'
-
-];
 
 
 function routeToController($uri, $routes)
@@ -28,5 +19,7 @@ function abort($code = 404){
 	require "views/{$code}.php";
 	die();
 }
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
