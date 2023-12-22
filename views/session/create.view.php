ec52e46f28c0ca33f1efccd598fc0c3544b0e7f4
Alkhatep <abdulla.sayed94@gmail.com>
2023-12-22 17:40:57 +0200

    Flash old form data

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -12,7 +12,7 @@
       <form class="space-y-6" action="/session" method="POST">
         <div>
           <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email">
+            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email" value="<?= old('email') ?>">
 
             <?php if (isset($errors['email'])) : ?>
               <p class="text-red-500 text-xs mt-2"><?= $errors['email'] ?></p>
